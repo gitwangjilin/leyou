05033ee55327971de73e6d12a51d7b3856dfea8c
wangjilin <wangjilin@chinadaas.com>
2019-07-30 17:28:03 +0800

    完整项目

---
+++ ly-api-gateway/src/main/java/com/leyou/LyApiGateway.java
@@ -0,0 +1,22 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Description:
+ * zuul网关
+ *
+ * @author: WangJiLin
+ * @Date: 2019-07-30 16:56
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class LyApiGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(LyApiGateway.class, args);
+    }
+}
---
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemService.java
@@ -0,0 +1,20 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Description:
+ * `ly-item-service启动项
+ *
+ * @author: WangJiLin
+ * @Date: 2019-07-30 17:06
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LyItemService {
+    public static void main(String[] args) {
+        SpringApplication.run(LyItemService.class, args);
+    }
+}
