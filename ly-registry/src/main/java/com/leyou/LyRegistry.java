c5f97db8e19bd1d6903ac52075a78ac3b8998d47
wangjilin <wangjilin@chinadaas.com>
2019-07-30 17:00:29 +0800

    网关和eurekaserver

---
+++ ly-registry/src/main/java/com/leyou/LyRegistry.java
@@ -0,0 +1,20 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Description:
+ * EurekaServer启动项
+ *
+ * @author: WangJiLin
+ * @Date: 2019-07-30 16:53
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class LyRegistry {
+    public static void main(String[] args) {
+        SpringApplication.run(LyRegistry.class, args);
+    }
+}
